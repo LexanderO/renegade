1567ec0b2b1317f2ecaa7c7b47120e7fc8b44662
LexanderO <alexanderowens1993@gmail.com>
2024-03-07 12:58:46 +0000

    Added Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p>Hello World</p>
+      <BrowserRouter>
+        <p>Hello World</p>
+      </BrowserRouter>
     </>
   );
 }
