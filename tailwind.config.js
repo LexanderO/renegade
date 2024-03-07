efd48ff58e0130fec93c19ae3a49f8f81260f126
LexanderO <alexanderowens1993@gmail.com>
2024-03-07 13:45:46 +0000

    added color theme and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    colors: {
+      platinum: "#E5E5E5",
+      black: "#030303",
+      blackbean: "#390B10",
+      burgundy: "#70131C",
+      madder: "#A61B29",
+    },
+    fontFamily: {
+      yellowtail: ["Yellowtail", "cursive"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
