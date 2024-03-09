fe97061373ee8e9d6259ec5a43c2c149d301a9a1
LexanderO <alexanderowens1993@gmail.com>
2024-03-09 03:34:18 +0000

    added more content to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,26 @@
 export const navLinks = [
-    {
-      id: "about",
-      text: "About",
-    },
-    {
-      id: "gallery",
-      text: "gallery",
-    },
-    {
-      id: "contact",
-      text: "Contact",
-    },
-  ];
+  {
+    id: "about",
+    text: "About",
+  },
+  {
+    id: "gallery",
+    text: "Gallery",
+  },
+  {
+    id: "contact",
+    text: "Contact",
+  },
+];
+
+export const aboutDescriptions = [
+  {
+    title: "Installation",
+    description: "Quick guide on setup the project before development and deployment",
+    steps: [
+      "Open command line",
+      "Navigate to the project directory: `cd renegade`",
+      "Install dependencies: `npm install`",
+    ],
+  },
+];
