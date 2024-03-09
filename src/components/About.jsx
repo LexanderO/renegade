0932a25abf1a57aab0556c4882eaaaa5a81fc7a7
LexanderO <alexanderowens1993@gmail.com>
2024-03-09 03:34:50 +0000

    mapping info content from constants to About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,22 @@
 import { useRef } from "react";
 
 import useScrollVisibility from "../hooks/useScrollVisibility.js";
+import { navLinks, aboutDescriptions } from "../constants";
 import { RedGirl } from "../assets";
+
+const TileCard = ({ index, title, description, steps }) => (
+  <div className="border-platinum border-r-8 bg-burgundy my-5">
+    <h3 className="text-xl text-right p-5 font-bold text-platinum">{title}</h3>
+    <p className="text-lg pb-4 text-black font-bold px-10">{description}</p>
+    <ol className="list-disc px-10">
+      {steps.map((step, index) => (
+        <li key={index} className="text-lg text-plantinum p-1">
+          {step}
+        </li>
+      ))}{" "}
+    </ol>
+  </div>
+);
 
 const About = () => {
   const ref = useRef();
@@ -9,23 +24,36 @@ const About = () => {
 
   return (
     <section
-      id="about"
+      id={navLinks[0].id}
       ref={ref}
-      className={`transition-opacity ease-in duration-700 ${
+      className={`transition-opacity ease-in duration-700 mt-24 ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
     >
       <div
         className="flex flex-wrap justify-center skew-y-3
-       bg-burgundy border-black border-b-8 pt-11"
+       bg-burgundy bg-opacity-95 border-black border-b-8 pt-11"
       >
-        <div className="-skew-y-6 w-1/2 bg-madder">
-          <p className="font-montserrat text-2 text-platinum"></p>
+        <div className="-skew-y-6 2xl:w-2/4 xl:w-3/4 md:w-3/4 sm:3/4 bg-madder px-11">
+          <div className="skew-y-3 flex flex-col -mr-16">
+            <h2 className="text-4xl -skew-x-3 text-right  bg-platinum p-5 font-bold text-grey font-yellowtail">
+              {navLinks[0].text}
+            </h2>
+            {aboutDescriptions.map((aboutDescription, index) => (
+              <TileCard
+                key={aboutDescription.title}
+                index={index}
+                description={aboutDescription.description}
+                steps={aboutDescription.steps}
+                {...aboutDescription}
+              />
+            ))}
+          </div>
         </div>
         <img
           src={RedGirl}
           alt="Red Girl"
-          className="object-scale-down w-[30%] self-end"
+          className="object-scale-down 2xl:w-1/4 xl:w-1/4 md:w-3/4 sm:3/4 mt-5 self-end"
         />
       </div>
     </section>
