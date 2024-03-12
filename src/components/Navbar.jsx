3ebb08ef4405698aeadc0d7e903d22d33fd001a9
LexanderO <alexanderowens1993@gmail.com>
2024-03-12 03:38:20 +0000

    added mobile menu for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="sticky top-10 z-50 flex flex-row-reverse">
       <div
-        className="flex flex-row justify-end items w-full
+        className="flex flex-row justify-end items w-full h-16
        bg-madder bg-opacity-75
        skew-y-3
        border-b-4 border-black
        "
       >
-        <ul className=" flex flex-row divide-x-4 divide-black sm:mr-24 md:mr-52">
+        <ul className="hidden md:flex flex-row divide-x-4 divide-black sm:mr-24 md:mr-52">
           {navLinks.map((navLink) => (
             <li key={navLink.id} className="">
               <a
@@ -23,7 +31,36 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        {/* Mobile Navigation Items */}
+        <div onClick={handleMenu} className="md:hidden mr-4 self-center">
+          {showMenu ? (
+            <span className="text-4xl">X</span>
+          ) : (
+            <span className="text-4xl">&#9776;</span>
+          )}
+        </div>
+       
       </div>
+      <ul
+          className={
+            showMenu
+              ? "fixed md:hidden left-0 top-0 w-[60%] h-full bg-madder/20 drop-shadow-2xl backdrop-blur-xl ease-in-out duration-700"
+              : "ease-in-out w-[60%] duration-700 fixed top-0 bottom-0 left-[-100%]"
+          }
+        >
+          {navLinks.map((navLink) => (
+            <li key={navLink.id} className="my-11 ml-5">
+              <a
+                href={"#" + navLink.id}
+                onClick={handleMenu}
+                className="text-platinum p-4 block uppercase hover:text-grey 
+             cursor-pointer duration-300 font-montserrat font-bold"
+              >
+                {navLink.text}
+              </a>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 };
